e9b4faea05b5b934a9b2752c71b8504c1ad43a73
rinesh.km <rinesh.km@accionlabs.com>
2021-01-09 15:00:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
