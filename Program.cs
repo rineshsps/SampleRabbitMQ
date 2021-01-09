ee68404105079689db7ecfb776620bb073eb157c
rinesh.km <rinesh.km@accionlabs.com>
2021-01-09 15:06:12 +0530

    Creating Exchange

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 
 namespace SampleRabbitMQ
 {
@@ -6,7 +7,23 @@ namespace SampleRabbitMQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+            //Main entry point to the RabbitMQ .NET AMQP client
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+            Console.WriteLine("Creating Exchange");
+            // Create Exchange
+            model.ExchangeDeclare("demoExchange1", ExchangeType.Direct);
+            model.ExchangeDeclare("demoExchange-Equalsv1", ExchangeType.Headers);
+            Console.ReadLine();
         }
     }
 }
