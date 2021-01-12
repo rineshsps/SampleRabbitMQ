d384e8c13cc3a93dc1b4219c50c27f640abd7f04
rinesh.km <rinesh.km@accionlabs.com>
2021-01-12 14:04:39 +0530

    Message read, publish

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using RabbitMQ.Client;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
 
 namespace SampleRabbitMQ
 {
@@ -20,9 +23,42 @@ namespace SampleRabbitMQ
             var connection = connectionFactory.CreateConnection();
             var model = connection.CreateModel();
             Console.WriteLine("Creating Exchange");
+
             // Create Exchange
-            model.ExchangeDeclare("demoExchange1", ExchangeType.Direct);
-            model.ExchangeDeclare("demoExchange-Equalsv1", ExchangeType.Headers);
+            model.ExchangeDeclare("demoExchange-v1", ExchangeType.Direct);
+            //model.ExchangeDeclare("demoExchange-Equalsv1", ExchangeType.Headers);
+
+            //Create Queue in RabbitMQ 
+            model.QueueDeclare("demoqueue-v1", true, false, false, null);
+            Console.WriteLine("Creating Queue");
+
+            // Bind Queue to Exchange
+            model.QueueBind("demoqueue-v1", "demoExchange-v1", "directexchange_key");
+            Console.WriteLine("Creating Binding");
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            var json = JsonConvert.SerializeObject(new { name = "Jhon", age = 12, DateCreate = DateTime.Now });
+            var body = Encoding.UTF8.GetBytes(json);
+
+            //byte[] messagebuffer = Encoding.Default.GetBytes("Direct Message");
+            model.BasicPublish("demoExchange-v1", "directexchange_key", properties, body);
+
+            Console.WriteLine("Message Sent");
+
+
+            //Read messages 
+            var consumer = new EventingBasicConsumer(model);
+            consumer.Received += (v, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            //autoAck: true - delete the messages from Queue
+            model.BasicConsume(queue: "demoqueue-v1", autoAck: false, consumer: consumer);
+
             Console.ReadLine();
         }
     }
